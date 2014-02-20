60b742ac372da53e4ae1472083b85e9e8b725166
Kelsey Berg <kberg@eagles.ewu.edu>
2014-02-20 14:30:42 -0800

    2

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,15 @@ $(document).foundation();
 // Your Awesome Scripts!
 $(document).ready(function(){
 
-	console.log('Page Loaded. Lets Do this!');
+	var msg1 = 'Hello!';
+		var msg2 = 'Hola!';
+			var msg3 = 'Bonjour!';
+
+		var msg2 = 'YO!!';
+
+
+	$('h1').text(msg2);
+
+	console.log(msg2);
 
 }); 
