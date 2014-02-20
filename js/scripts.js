bb5b5cb3c71958818e98a5c717be341f2003ab17
Kelsey Berg <kberg@eagles.ewu.edu>
2014-02-20 14:38:34 -0800

    4

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,17 @@ $(document).foundation();
 // Your Awesome Scripts!
 $(document).ready(function(){
 
-	var msg1 = 'Hello!';
-		var msg2 = 'Hola!';
-			var msg3 = 'Bonjour!';
+	// returns 0 to 'limit'
+	var limit = 2;
+	var numRand = Math.floor(Math.random()*limit);
+
+	var msg0 = 'Hello!';
+		var msg1 = 'Hola!';
+			var msg2 = 'Bonjour!';
 
 
-	$('h1').text(msg2);
+	$('h1').text('msg' + numRand);
 
-	console.log(msg2);
+	console.log('msg' + numRand);
 
 }); 
